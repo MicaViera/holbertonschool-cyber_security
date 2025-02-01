fdbb60b7911ffde3335ecc2ae89c2997c3b52985
MicaViera <vieranoelia98@gmail.com>
2025-02-01 18:36:35 -0500

    Write a Ruby function that reads content from a Json file and count the userId.

---
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+require 'json'
+# Write a Ruby function that reads content from a Json file.
+
+def count_user_ids(path)
+  read_file = File.read(path)
+  data = JSON.parse(read_file)
+  id_count = Hash.new(0)
+  
+  data.each do |entry|
+    id_count[entry["userId"]] += 1 if entry.key?("userId")
+  end
+  
+  id_count
+end
